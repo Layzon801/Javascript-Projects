1338dc1dba24582bf3448ac045119df00455c44c
Layzon801 <layzonhair@gmail.com>
2020-12-02 07:28:30 -0800

    made the index page and the main javascript page

---
+++ Basic-Javascript-Projects/Project1_expressions_alert/JS/Main.js
@@ -0,0 +1,11 @@
+var X='Hello, World!'; // Defined variable X and gave it a string value. //
+window.alert(X); // Invoked a window displaying variable X //
+var V='\"I want to go to sleep.\" Auroura said' + ' \"But we have so much work to do.\" Legacy responded<br>'; // Defined variable V and gave it a concatenated string value. //
+document.write(V); // displayed variable V on the html document //
+var Family = "The Kirigayas", Dad = "Kazuto", Mom = "Asuna", Daughter = "Yui"; // created the variables Family, Dad, Mom and Daughter. //
+var Dad = Dad.fontcolor("black"); // Set the text of variables Dad, Mom, and Daughter to display in the colors black, Orange and blue respectively. //
+var Mom = Mom.fontcolor("orange"); 
+var Daughter = Daughter.fontcolor("blue"); 
+document.write(Mom); // displayed variable Mom in the html document. //
+document.write("<br>") // put a line break in the text. //
+document.write(5*12); // displayed the result of the equation. //
